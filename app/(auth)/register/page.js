18473bfc5872d074c4be676a21b204f436809bd2
Developer-Sohail786 <sanu30963@gmail.com>
2026-02-28 13:49:53 +0530

    register

--- app/(auth)/register/page.js
+++ app/(auth)/register/page.js
@@ -1,9 +1,14 @@
 export const dynamic = "force-dynamic";
-import { Suspense } from "react";
-import Register from "./register-Component";
+// import { Suspense } from "react";
+// import Register from "./register-Component";
 
-export default function Page(){
-  <Suspense>
-    <Register/>
-  </Suspense>
+// export default function Page(){
+//   <Suspense>
+//     <Register/>
+//   </Suspense>
+// }
+export const dynamic = "force-dynamic";
+
+export default function Page() {
+  return <div>REGISTER PAGE TEST</div>;
 }
