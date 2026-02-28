5c3f2b29878b5cb16090f5c01f4ff5f5b240449c
Developer-Sohail786 <sanu30963@gmail.com>
2026-02-28 14:05:21 +0530

    layout

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,35 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import Providers from "./Components/Provider";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Short your URL's",
+  description: "Trusted URL shorterner",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-purple-50`} suppressHydrationWarning
+      >
+        <Providers>
+
+
+          {children}
+        </Providers>
       </body>
     </html>
   );
