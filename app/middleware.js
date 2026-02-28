76937f120045ffbf7bd4a32ddfedb0b72045b13c
Developer-Sohail786 <sanu30963@gmail.com>
2026-02-28 13:54:44 +0530

    middleware

--- app/middleware.js
+++ app/middleware.js
@@ -1,17 +1,21 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
 
-export async function middleware(req) {
-  const token = await getToken({ req });
-  const { pathname } = req.nextUrl;
+// export async function middleware(req) {
+//   const token = await getToken({ req });
+//   const { pathname } = req.nextUrl;
 
-  if (!token && pathname.startsWith("/shorten")) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+//   if (!token && pathname.startsWith("/shorten")) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
 
-  return NextResponse.next();
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/shorten"],
+// };
+
+export function middleware() {
+  return;
 }
-
-export const config = {
-  matcher: ["/shorten"],
-};
