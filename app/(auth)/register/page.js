7b50eb85e80ca5b053245c8625d56b5d215e5914
Developer-Sohail786 <sanu30963@gmail.com>
2026-03-01 12:13:24 +0530

    SuSpense return fix

--- app/(auth)/register/page.js
+++ app/(auth)/register/page.js
@@ -3,8 +3,10 @@ export const dynamic = "force-dynamic";
 import { Suspense } from "react";
 import Register from "./register-component";
 
-export default function Page(){
-  <Suspense>
-    <Register/>
-  </Suspense>
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Register />
+    </Suspense>
+  );
 }
