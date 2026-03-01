465a44ea17ab3f519cbdd8d7a47b80a41f124309
Developer-Sohail786 <sanu30963@gmail.com>
2026-03-01 11:52:57 +0530

    fix register-component

--- app/(auth)/register/page.js
+++ app/(auth)/register/page.js
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
 
 import { Suspense } from "react";
-import Register from "./register-Component";
+import Register from "./register-component";
 
 export default function Page(){
   <Suspense>
